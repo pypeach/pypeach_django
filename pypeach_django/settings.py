74a94ce888f2dca956fa5eb799dc77ea79936f65
takashi-ebinuma <tz2i5i.ebinuma@gmail.com>
2018-12-25 18:20:59 +0900

    sqllite3を追加

--- pypeach_django/settings.py
+++ pypeach_django/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'pypeach_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +76,7 @@ WSGI_APPLICATION = 'pypeach_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
     }
 }
 
