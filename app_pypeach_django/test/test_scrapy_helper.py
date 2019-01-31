fc3820cefc1b9f72ca27267e9579e70ea506dc09
takashi-ebinuma <tz2i5i.ebinuma@gmail.com>
2019-01-31 16:55:11 +0900

    Scrapyのヘルパーを追加

---
+++ app_pypeach_django/test/test_scrapy_helper.py
@@ -0,0 +1,40 @@
+import logging
+
+from app_pypeach_django.application.helper.scrapy_helper import ScrapyHelper, HttpErrorException, ScrapyIllegalException
+from app_pypeach_django.test.test_base import UnitTestBase
+
+"""
+scrapyのテストです
+"""
+__author__ = "t.ebinuma"
+__version__ = "1.0"
+__date__ = "31 January 2019"
+
+
+class TestScrapyHelper(UnitTestBase):
+
+    def test_get_html_001(self):
+        """
+        htmlの取得を検証するテストケースです
+        """
+        html = ScrapyHelper.get_html('http://mocjax.com/example/scrape/', True)
+        logging.debug("html={}".format(html))
+
+        self.assertEqual(len(html.findAll('div')), 14)
+        self.assertEqual(len(html.findAll('h1')), 4)
+
+    def test_get_html_002(self):
+        """
+        htmlの取得(異常)を検証するテストケースです
+        """
+        # 存在しないURL(404)を指定する
+        with self.assertRaises(HttpErrorException):
+            ScrapyHelper.get_html('http://mocjax.com/example/scrape2/')
+
+        # 誤ったURL指定になっている
+        with self.assertRaises(ScrapyIllegalException):
+            ScrapyHelper.get_html('//mocjax.com/pypeach/pypeach-example/example.html')
+        with self.assertRaises(ScrapyIllegalException):
+            ScrapyHelper.get_html('http//example.com')
+        with self.assertRaises(ScrapyIllegalException):
+            ScrapyHelper.get_html('http//192.168.10.10')
